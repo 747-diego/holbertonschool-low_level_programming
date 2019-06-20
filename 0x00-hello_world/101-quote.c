472a8dc114498126a6f9240c5471ff239061f4eb
747-diego <725@holbertonschool.com>
2019-06-20 14:00:51 +0000

    Updated BETTY checks, trailing white space and indents have been taking care of

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 
-  int quote;
+	int quote;
 
-    quote = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	q = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", quote);
-  return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", q);
+	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main function to printf string using printf 
+ * main - main function to printf string using printf
  *
- * return: Return: Always 0 (Success)
+ * Return: Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
