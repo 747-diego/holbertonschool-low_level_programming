b89e605283b3a010d89c3ddbd2f92ceb23dc70f4
747-diego <725@holbertonschool.com>
2019-06-20 22:37:01 +0000

    Added a second variable called last for last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,20 +19,21 @@ int main(void)
 
 	/* your code goes there */
 
+	last = n % 10;
 
 	if (last > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last % 10);
+	printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
 
 	else if (last == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, last % 10);
+	printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
