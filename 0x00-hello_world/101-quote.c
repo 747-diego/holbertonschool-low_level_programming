fd107567c6fd14781705035df75f52c972a54be2
747-diego <725@holbertonschool.com>
2019-06-20 14:18:26 +0000

    Updated BETTY checks, trailing white space and indents have been taking care of

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,8 +13,8 @@ int main(void)
 
 	int q;
 
-	q = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	q = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", q);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", q);
 	return (1);
 }
