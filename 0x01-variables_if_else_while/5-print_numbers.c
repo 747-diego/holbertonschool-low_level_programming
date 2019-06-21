b1ea4802dbb2f05df4b332e53beadbcf2b4ed93f
747-diego <725@holbertonschool.com>
2019-06-21 02:31:15 +0000

    created a for loop along with a printf function to display numbers 0 through 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - main function that prints numbers 0 through 9
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	printf("%d", num);
+	putchar ('\n');
+	return (0);
+}
