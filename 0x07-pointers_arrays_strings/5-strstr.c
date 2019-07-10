9eb4ee3cca45604bdd0d486fe5d036a27b71bc3f
747-diego <725@holbertonschool.com>
2019-07-10 03:48:33 +0000

    ADDED README FILE and also final edits

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the beginning of the located substring
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+
+{
+
+
+	while (*haystack != '\0')
+
+	{
+
+		char *iterationONE = haystack;
+		char *iterationTWO = needle;
+
+
+		while (*iterationTWO == *haystack && *iterationTWO != '\0'
+		       && *haystack != '\0')
+		{
+			haystack++;
+			iterationTWO++;
+		}
+
+
+		if (*iterationTWO == '\0')
+
+		{
+			return (iterationONE);
+		}
+
+		haystack = iterationONE + 1;
+
+	}
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /* Prototypes below */
 int _putchar(char c);
