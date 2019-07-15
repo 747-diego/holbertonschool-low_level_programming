0f4b3e0cb4337f8da45bcc4a8b448dd6ecd981e6
747-diego <725@holbertonschool.com>
2019-07-15 03:59:18 +0000

    betty checked!

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+* main - a program that adds all arguments it receives
+* @argc: represent the number of arguments entered in the command line
+* @argv: an array that holds string values of whats entered in the command line
+* Return: zero
+*/
+
+int main(int argc, char *argv[])
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,38 @@
 #include "holberton.h"
 
 /**
-* main - a program that prints all arguments it receives
+* main - a program that adds all arguments it receives
 * @argc: represent the number of arguments entered in the command line
 * @argv: an array that holds string values of whats entered in the command line
 * Return: zero
 */
 
+
 int main(int argc, char *argv[])
 
 {
-	int iterate;
-	int sum = 0;
 
-	if (argc)
+        int iterate;
+        int sum = 1;
 
-	{
+        if (argc <= 2)
 
-		for (iterate = 0; iterate < argc; iterate++)
+        {
+                printf("Error\n");
+                return (1);
+        }
 
-		{
-			printf("%s\n", argv[iterate]);
-			sum += atoi(argv[iterate]);
-		}
-	}
-	return (0);
+        else
+
+        {
+                for (iterate = 1; iterate < argc; iterate++)
+
+                {
+                        sum += atoi(argv[iterate]);
+                }
+        }
+
+        printf("%d\n", sum);
+        return (0);
 }
+
