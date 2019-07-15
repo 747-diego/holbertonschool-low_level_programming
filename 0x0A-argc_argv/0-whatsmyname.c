de2f3322b2cda00f51d8034f266ebea6534e86bf
747-diego <725@holbertonschool.com>
2019-07-15 02:27:02 +0000

    betty checked, compiled under Holberton rules, also added comments up top

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 
 /**
-*
-*
-*
+* main - a program that prints its name, followed by a new line
+* @argc: represent the number of arguments entered in the command line
+* @argv: an array that holds string values of whats entered in the command line
+* Return: zero
 */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 
 {
 	int iterate;
 
-	for(iterate = 0; iterate < argc; iterate++)
+	for (iterate = 0; iterate < argc; iterate++)
 
 	{
 		printf("%s\n", argv[iterate]);
-		return (0);
 	}
+
+	return (0);
 }
