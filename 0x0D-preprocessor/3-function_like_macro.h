6b7e02d138ad67b5185703cafd2a38d9d39feee3
747-diego <725@holbertonschool.com>
2019-07-23 03:05:49 +0000

    Betty checked and added README file

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ABSOLUTE_HEADER
+#define ABSOLUTE_HEADER
+#define ABS(x) ((x < 0) ? (-x):(x));
+#endif
---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* Header Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+/* Prototypes below */
+int _putchar(char c);
+
+#endif
