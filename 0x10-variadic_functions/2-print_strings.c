2e0f84efdcb71638e9be9f3ebefca36ce6e51975
747-diego <725@holbertonschool.com>
2019-07-28 19:01:30 +0000

    Set iterate not equal to n - 1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		printf("%s", pointerCH);
 
-		if (iterate < n - 1 && separator)
+		if (iterate != n - 1 && separator)
 			printf("%s", separator);
 	}
 
