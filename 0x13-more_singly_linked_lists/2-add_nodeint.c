3ff250e1400e5f29d957abe5f034b5e133e77891
747-diego <725@holbertonschool.com>
2019-09-17 00:14:15 +0000

    [BETTY CHECK] Missing proper description tags

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+* add_nodeint - function that adds a new node at the beginning ofa listint list
+* @head: pointer to the begining of the list
+* @n: elements in the array
+* Return: the address of the new element, or NULL if it failed
+*/
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *NewNode;
