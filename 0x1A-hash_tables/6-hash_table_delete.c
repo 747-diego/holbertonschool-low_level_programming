b1b59aeb9bc027606561bb63f9478927d68e8408
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-20 01:37:07 -0500

    [MISSING A BLANK LINE AFTER DECLARATIONS]

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,8 +12,8 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t **Array;
 	unsigned int long TableSize = 0;
 	unsigned int long index = 0;
-	TableSize = ht->size;
 
+	TableSize = ht->size;
 	while (index < TableSize)
 	{
 		Array = ht->array;
