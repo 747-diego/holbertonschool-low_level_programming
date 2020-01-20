1e82c50a727dda70aa214350d9d766bff1447c7f
Diego Tardio <diegotardio@Diegos-MacBook-Pro.local>
2020-01-19 20:00:31 -0500

    [DELETED WHITE SPACE]

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (iteratation == index)
 		{
-    			return (head);
+			return (head);
 		}
 		head = head->next;
 	}
