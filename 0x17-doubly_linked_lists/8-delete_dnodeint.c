be50742bbaccb3d5cd67e925782dfba2638cc32c
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-10 23:38:37 -0400

    [TESTING 2]

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - a function that deletes the node at index of Dlist
+ * delete_dnodeint_at_index - function that deletes the node at index of Dlist
  * @head: head pointer points at 1st node
  * @index: is the index of the node tarting from 0
  * Return: 1 if it succeeded, -1 if it failed
@@ -10,14 +10,14 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *dnode;
-    dlistint_t *temp;
-    unsigned int iterator = 0;
-    unsigned int position = index;
+	dlistint_t *temp;
+	unsigned int iterator = 0;
+	unsigned int position = index;
 
 	if (*head == NULL)
 		return (-1);
 
-    temp = *head;
+	temp = *head;
 	if (position == 0)
 
 	{
@@ -25,20 +25,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp->next;
 
 		if (temp->next)
-
-        {
+		{
 			temp->next->prev = NULL;
-        }
+		}
 
 		free(dnode);
 		return (1);
 	}
 
-    while (temp != NULL)
-
-    {
+	while (temp != NULL)
+	{
 		if (iterator == position - 1 && temp->next)
-
 		{
 			dnode = temp->next;
 			temp->next = dnode->next;
@@ -51,10 +48,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
             free(dnode);
 			return (1);
 		}
-
     iterator++;
     temp = temp->next;
     }
-
 	return (-1);
 }
