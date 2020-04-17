d2a49fad69eb28740ec6d1c58322b0b5420f5cc1
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-16 22:10:18 -0400

    [SYNTAX ERROR]

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	index = 0;
-    while ( index < size )
+	while ( index < size )
 	{
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 		if (array[index] == value)
